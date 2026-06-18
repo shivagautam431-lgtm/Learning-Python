0cc3db782f5d1b000f06b857e774588e33adb16a
shivagautam431-lgtm <shivagautam431@gmail.com>
2026-06-18 10:55:44 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,6 @@
+price1 = float(input("Write the price of the first item"))
+price2 = float(input("Write the price of the second item"))
+
+total = price1 + price2
+
+print("it is the total bill: " + str(total))
