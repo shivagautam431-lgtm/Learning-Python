d3b6ffa57be22e8453d274d3dffde63e04d6c59d
shivagautam431-lgtm <shivagautam431@gmail.com>
2026-06-18 11:04:16 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,13 @@
-price1 = float(input("Write the price of the first item"))
-price2 = float(input("Write the price of the second item"))
+# Smart Bill Calculator
+price1 = float(input("Write the price of the first item: "))
+price2 = float(input("Write the price of the second item: "))
 
 total = price1 + price2
 
-print("it is the total bill: " + str(total))
+print("Your Total bill is: " + str(total))
+
+# Decision Making Process
+if total > 500:
+    print("Congratulations! The bill is more than 500, you will get a 10%  discount.")
+else:
+    print("Your bill is less than 500, no discount recevied.")
